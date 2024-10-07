4d9c32e08c1055e0f9f9c37957375f46e0c9042c
MB <161880835+Maoooa@users.noreply.github.com>
2024-10-07 22:58:33 +0700

    Update newpost.php and verify.php

--- verify.php
+++ verify.php
@@ -1,49 +1,26 @@
 <?php
-session_start();
-if (isset($_SESSION['id'])) {
-    header('location:index.php');
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
-    $login = $_POST["login"];
-    $pwd = $_POST["pwd"];
-    ?>
-    <?php
-    if ($_POST["login"] == "admin" && $_POST["pwd"] == "ad1234") {
-        $_SESSION["username"] = "admin";
-        $_SESSION["role"] = "a";
-        $_SESSION["id"] = session_id();
+    session_start();
+    if (isset($_SESSION['id'])) {
         header('location:index.php');
-        die();
-        //echo "ยินดีต้อนรับคุณ ADMIN ";
-        //echo "<a href="."index.php"."> <BR> กลับไปยังหน้าหลัก</a>";
-    } elseif ($_POST["login"] == "member" && $_POST["pwd"] == "mem1234") {
-        $_SESSION["username"] = "member";
-        $_SESSION["role"] = "m";
-        $_SESSION["id"] = session_id();
-        header('location:index.php');
-        die();
-        //echo "ยินดีต้อนรับคุณ MEMBER ";
-        //echo "<a href="."index.php"."> <BR> กลับไปยังหน้าหลัก</a>";
-    } else {
-        // echo "ชื่อบัญชีและรหัสผ่านไม่ถูกต้อง";
-        $_SESSION['error'] = 'error';
-        header('location:login.php');
         die();
     }
-    ?>
-
-
-</body>
-
-</html>
+    $login = $_POST['login'];
+    $pwd = $_POST['pwd'];
+    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+    $sql = "SELECT * FROM user where login='$login' and password = sha1('$pwd')";
+    $result = $conn->query($sql);
+    if($result->rowCount()==1){
+        $data=$result->fetch(PDO::FETCH_ASSOC);
+        $_SESSION['username']=$data['login'];
+        $_SESSION['role']=$data['role'];
+        $_SESSION['user_id']=$data['id'];
+        $_SESSION['id']=session_id();
+        header("location:index.php");
+        die();
+    }else{
+        $_SESSION['error']="error";
+        header("location:login.php");
+        die();
+    }
+    $conn=null;
+?>
