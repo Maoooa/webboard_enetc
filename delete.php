529f0ec8920a0b0c16fa3323c80d4923e27bea1c
Maoooa <nujra2546@gmail.com>
2024-10-11 16:59:43 +0700

    Update 10/11/2567
    Update 10/11/2567

--- delete.php
+++ delete.php
@@ -1,9 +1,18 @@
 <?php
-session_start();
-if(isset($_SESSION['id']) && ($_SESSION['role'] == 'a')){
-    echo "ลบกระทู้ หมายเลข $_GET[id]";
-}else{
-    header("location:index.php");
-    die();
-}
+    session_start();
+    if (isset($_SESSION['role']) && $_SESSION['role'] == 'a') {
+
+        $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+
+        $sql_post = "DELETE FROM post WHERE id = $_GET[id]";
+        $conn->exec($sql_post);
+        $sql_com = "DELETE FROM comment WHERE post_id = $_GET[id]";
+        $conn->exec($sql_com);
+
+        $conn = null;
+        header("location:index.php");
+    } else {
+        header("location:index.php");
+        die();
+    }
 ?>
--- index.php
+++ index.php
@@ -10,6 +10,13 @@ session_start();
     <title>Webboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- script -->
+    <script>
+        function myfunction() {
+            let r = confirm("ต้องการที่จะลบกระทู้นี้จริงหรือไม่") ;
+            return r;
+        } 
+    </script>
     <!-- icon -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
@@ -18,7 +25,7 @@ session_start();
     <div class="container-lg">
         <h1 style="text-align: center;" class="mt-3">Webboard KakKak</h1>
         <!--navbar -->
-        <?php include"nav.php" ?>
+        <?php include "nav.php" ?>
         <!-- หมวดหมู่ -->
         <label class="mt-3">หมวดหมู่ :</label>
         <span class="dropdown">
@@ -31,33 +38,37 @@ session_start();
                 <li><a class="dropdown-item" href="#">เรื่องเรียน</a></li> -->
                 <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
                 <?php
-                    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-                    $sql = "SELECT * FROM category";
-                    foreach($conn->query($sql) as $row){
-                        echo "<li><a class=dropdown-item href=#>$row[name]</a></li>";
-                    }
-                    $conn=null;
+                $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+                $sql = "SELECT * FROM category";
+                foreach ($conn->query($sql) as $row) {
+                    echo "<li><a class=dropdown-item href=#>$row[name]</a></li>";
+                }
+                $conn = null;
                 ?>
             </ul>
-            <?php 
-                if(isset($_SESSION['id'])){
-                    echo  "<a href='newpost.php' class='btn btn-success float-end mt-3'>
+            <?php
+            if (isset($_SESSION['id'])) {
+                echo  "<a href='newpost.php' class='btn btn-success float-end mt-3'>
                     <i class='bi bi-plus-lg'></i>สร้างกระทู้ใหม่ </a>";
-                }
+            }
             ?>
         </span>
-        
+
         <form action="post.php" method="get">
             <table class="table table-striped mt-4">
                 <?php
-                $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-                $sql="SELECT t3.name,t1.title,t1.id,t2.login,t1.post_date FROM post as t1
+                $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+                $sql = "SELECT t3.name,t1.title,t1.id,t2.login,t1.post_date FROM post as t1
                 INNER JOIN user as t2 ON (t1.user_id=t2.id)
                 INNER JOIN category as t3 ON (t1.cat_id=t3.id) ORDER BY t1.post_date DESC";
                 $result = $conn->query($sql);
-                while($row = $result->fetch()){
+                while ($row = $result->fetch()) {
                     echo "<tr><td>[ $row[0] ] <a href=post.php?id=$row[2]
-                    style=text-decoration:none>$row[1]</a><br>$row[3]-$row[4]</td></tr>";
+                    style=text-decoration:none>$row[1]</a>";
+                    if(isset($_SESSION['id']) && $_SESSION['role']=='a'){
+                        echo " <a onclick='return myfunction()' class='btn btn-danger' style='float:right' role='button' href=delete.php?id=$row[2]><i class='bi bi-trash'></i></a>";
+                    }
+                    echo "<br>$row[3]-$row[4]</td></tr>";
                 }
                 $conn = null;
                 ?>
@@ -65,6 +76,7 @@ session_start();
             </table>
         </form>
         </table>
+        
 
     </div>
 
